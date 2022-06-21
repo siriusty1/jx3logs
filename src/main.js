484ca44be91cf738202f163ee1478899a13aa1db
ssHss <siriustao@163.com>
2022-06-21 17:38:02 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,30 @@ import App from './App.vue'
 import router from './router'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/theme-chalk/dark/css-vars.css'
+import ECharts from 'vue-echarts'
+import { use } from "echarts/core";
+
+import {
+    CanvasRenderer
+} from 'echarts/renderers'
+import {
+    LineChart
+} from 'echarts/charts'
+import {
+    GridComponent,
+    TooltipComponent,
+    DatasetComponent,
+    VisualMapComponent
+} from 'echarts/components'
+
+use([
+    CanvasRenderer,
+    LineChart,
+    GridComponent,
+    TooltipComponent,
+    DatasetComponent,
+    VisualMapComponent
+]);
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
@@ -10,5 +34,5 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 
 app.use(router)
-
+app.component('v-chart',ECharts)
 app.mount('#app')
