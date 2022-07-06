60e59b8d936403e4ab5db568dec1935f027b18dc
siriustao <siriustao@163.com>
2022-07-07 04:05:05 +0800

    7.6

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM nginx
+MAINTAINER siriustao
+RUN rm /etc/nginx/conf.d/default.conf
+ADD default.conf /etc/nginx/conf.d/
+COPY dist/ /usr/share/nginx/html/
--- src/pages/Ranking.vue
+++ src/pages/Ranking.vue
@@ -3,14 +3,6 @@
 </template>
 
 <script setup>
-import axios from "axios";
-
-// axios({
-//   method:'get',
-//   url: `http://139.199.102.41:8009/getRank?map=${}&boss=${}&occ${sect}&page=${}`
-// }).then((res)=>{
-//   console.log(res)
-// })
 
 </script>
 
